15722b98d6b8ef17ef13eaeabd6dcad1a03aa072
Ilia K <ki.stfu@gmail.com>
2018-07-09 00:16:26 +0200

    Remove gsl_parse_incipit() and gsl_get_dir_size() from master

--- include/gsl-parser.h
+++ include/gsl-parser.h
@@ -17,14 +17,3 @@ extern gsl_err_t gsl_parse_cdata(void *obj, const char *rec, size_t *total_size)
 extern gsl_err_t gsl_parse_task(const char *rec, size_t *total_size,
                                 struct gslTaskSpec *specs, size_t num_specs);
 
-extern gsl_err_t gsl_parse_incipit(const char *rec,
-				   size_t rec_size,
-				   char *result_tag_name,
-				   size_t *result_tag_name_size,
-				   char *result_name,
-				   size_t *result_name_size);
-extern gsl_err_t gsl_get_dir_size(const char *rec,
-                                  size_t rec_size,
-                                  const char **val,
-                                  size_t *val_size,
-                                  size_t *total_trailer_size);
--- include/gsl-parser/config.h
+++ include/gsl-parser/config.h
@@ -1,5 +1,4 @@
 #pragma once
 
 #define GSL_NUM_ENCODE_BASE 10
-#define GSL_DIR_ENTRY_SIZE 64
 
